a7eb40cca9f1a24fe6c88b81aa41587d76fc3e7c
Eden Park <edengoforit@gmail.com>
2026-03-26 19:34:23 +1300

    add: create ContactPage component to render Contact section

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Contact from "@/components/contact";
+
+export default function ContactPage(): React.JSX.Element {
+  return (
+    <main>
+      <Contact />
+    </main>
+  );
+}
