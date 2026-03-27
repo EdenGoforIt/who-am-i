1c18f255c24ebab5efdf24ca61976b14bc0edb12
Eden Park <edengoforit@gmail.com>
2026-03-27 18:15:31 +1300

    add: create ExperiencePage component to render Experience section

---
+++ app/experience/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Experience from "@/components/experience";
+
+export default function ExperiencePage(): React.JSX.Element {
+  return (
+    <main>
+      <Experience />
+    </main>
+  );
+}
