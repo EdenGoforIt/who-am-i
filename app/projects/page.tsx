5d9677925f1a97a99009d3c70fff5679538f3ce4
Eden Park <edengoforit@gmail.com>
2026-03-28 19:45:09 +1300

    add: create ProjectsPage component to render Projects section

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Projects from "@/components/projects";
+
+export default function ProjectsPage(): React.JSX.Element {
+  return (
+    <main>
+      <Projects />
+    </main>
+  );
+}
