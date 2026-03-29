7e3dc4f4ce8c5968a9f151cb85801e6c91dbaae7
Eden Park <edengoforit@gmail.com>
2026-03-29 17:48:40 +1300

    add: create SkillsPage component to render Skills section

---
+++ app/skills/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Skills from "@/components/skills";
+
+export default function SkillsPage(): React.JSX.Element {
+  return (
+    <main>
+      <Skills />
+    </main>
+  );
+}
