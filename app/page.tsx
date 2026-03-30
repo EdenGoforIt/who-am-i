b04f48454899acbca4ca464fe6b0c636621b38a0
Eden Park <edengoforit@gmail.com>
2026-03-30 20:19:01 +1300

    remove: eliminate Nav component from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Nav from "@/components/nav";
 import Hero from "@/components/hero";
 import About from "@/components/about";
 import Projects from "@/components/projects";
@@ -10,7 +9,6 @@ import Contact from "@/components/contact";
 export default function Home(): React.JSX.Element {
   return (
     <main>
-      <Nav />
       <Hero />
       <About />
       <Projects />
